76431350cdc537d9eec671a13bc8e695c405aaea
Benjamin1243 <U2486886@rts-365.dk>
2025-01-09 12:37:06 +0100

    jerjejrjer

---
+++ dataBase.js
@@ -0,0 +1,27 @@
+import { MongoClient } from "mongodb";
+import express from "express"
+import ViteExpress from "vite-express"
+
+
+const DATABASE_URL = "mongodb+srv://benjismi06:AiEDFRTsJHHt7b68@twitter.vz9z2.mongodb.net/?retryWrites=true&w=majority&appName=Twitter"
+
+
+const app = express()
+
+const client = new MongoClient(DATABASE_URL);
+const database = client.db('Twitter');
+
+
+
+
+
+
+app.get("/message", async(_, res) => res.send("erer"));
+
+app.get("/api/post", async(_,response) => {
+    const tweets = database.collection('post');
+    
+    response.json(await tweets.find().toArray())
+})
+
+ViteExpress.listen(app, 3000, async() => console.log("server is running"));
---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import App from './App.jsx'
+
+import { BrowserRouter, Routes, Route } from "react-router";
+import Next from './Next.jsx';
+
+
+
+createRoot(document.getElementById('root')).render(
+  
+  <StrictMode>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/next" element={<Next />} />
+    </Routes>
+  </BrowserRouter>
+  </StrictMode>,
+)
